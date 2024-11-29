5a480c2a11bf5ff563e5ffa3f0190a326c09711e
Jack Reichert <jack@aryaworks.com>
2024-11-29 17:31:22 -0600

    responsive

--- script.js
+++ script.js
@@ -4,11 +4,11 @@ document.getElementById('signupForm').addEventListener('submit', function(e) {
     const email = document.getElementById('email').value;
     const errorElement = document.getElementById('emailError');
     
-    // Regular expression for academic email domains
-    const academicEmailRegex = /^[a-zA-Z0-9._%+-]+@(?:[a-zA-Z0-9-]+\.)*(?:edu|ac\.uk|edu\.au|ac\.nz|edu\.cn|ac\.jp|edu\.sg|ac\.za|edu\.br|ac\.in)$/i;
+    // Regular expression for academic email domains - expanded for international institutions
+    const academicEmailRegex = /^[a-zA-Z0-9._%+-]+@(?:[a-zA-Z0-9-]+\.)*(?:edu|ac\.il|ac\.[a-z]{2}|edu\.[a-z]{2}|uni-[a-z]+\.[a-z]{2}|university\.[a-z]{2}|polytechnic\.[a-z]{2}|institute\.[a-z]{2}|hochschule\.[a-z]{2}|universite\.[a-z]{2}|universidad\.[a-z]{2}|universita\.[a-z]{2}|universiteit\.[a-z]{2}|universitet\.[a-z]{2})$/i;
     
     if (!academicEmailRegex.test(email)) {
-        errorElement.textContent = 'Please use an academic email address (.edu, .ac.uk, etc.)';
+        errorElement.textContent = 'Please use an academic email address (e.g. .edu, .ac.uk, .edu.au, uni-*.de, etc.)';
         return;
     }
     
