775882401b5d750776049ffb9ac739a54c5db1bf
Jack Reichert <jack@aryaworks.com>
2024-12-01 16:43:17 -0600

    new worker

---
+++ email-worker/worker.js
@@ -0,0 +1,14 @@
+export default {
+  async email(message, env, ctx) {
+    // Log incoming email for debugging
+    console.log('Received email:', {
+      from: message.from,
+      to: message.to,
+      subject: message.subject
+    });
+
+    // For now, just acknowledge receipt
+    // You can add more processing logic here later if needed
+    return {};
+  }
+};
